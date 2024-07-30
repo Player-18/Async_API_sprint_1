075984e7900637f4b5052a1fb7bddad232607fb6
NikolaySolop <nikolay.solop@yandex.ru>
2024-07-31 00:08:37 +0300

    fix to ensure elastic search dependencies works  - added missing ELASTIC_SCHEMA variable in config,py  - added ELASTIC_HOST and ELASTIC_PORT variables for elsaticseach to env.example  - uncommented startup and shutdown procedures in main.py

--- src/core/config.py
+++ src/core/config.py
@@ -19,3 +19,5 @@ ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
 # Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ELASTIC_SCHEMA = 'http://'
--- src/core/main.py
+++ src/core/main.py
@@ -32,20 +32,20 @@ app.add_middleware(
 )
 
 
-# @app.on_event('startup')
-# async def startup():
-#     # Подключаемся к базам при старте сервера
-#     # Подключиться можем при работающем event-loop
-#     # Поэтому логика подключения происходит в асинхронной функции
-#     redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
-#     elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_SCHEMA}{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
-#
-#
-# @app.on_event('shutdown')
-# async def shutdown():
-#     # Отключаемся от баз при выключении сервера
-#     await redis.redis.close()
-#     await elastic.es.close()
+@app.on_event('startup')
+async def startup():
+    # Подключаемся к базам при старте сервера
+    # Подключиться можем при работающем event-loop
+    # Поэтому логика подключения происходит в асинхронной функции
+    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_SCHEMA}{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    # Отключаемся от баз при выключении сервера
+    await redis.redis.close()
+    await elastic.es.close()
 
 
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
