9dcc00ed0744c3f957c71a8ac43c9279e02ee624
Artem Volinets <artemvol1998@gmail.com>
2024-08-04 23:29:24 +0300

    Fix ES .env.

--- src/core/config.py
+++ src/core/config.py
@@ -14,9 +14,9 @@ REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
 # Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
-ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
-ELASTIC_SCHEMA = os.getenv('ELASTIC_SCHEMA', 'http://')
+ELASTIC_HOST = os.getenv('ES_HOST', '127.0.0.1')
+ELASTIC_PORT = int(os.getenv('ES_PORT', 9200))
+ELASTIC_SCHEMA = os.getenv('ES_SCHEMA', 'http://')
 
 # Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
