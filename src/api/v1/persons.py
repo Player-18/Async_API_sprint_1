5219c1fa8a32c0940f1265fa8897278f8291e883
Artem Volinets <artemvol1998@gmail.com>
2024-08-05 12:40:39 +0300

    Add HTTPException. Add FastAPI doc.

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, Path
 from fastapi_cache.decorator import cache
 
 from models.film import FilmListOutput
@@ -17,9 +17,9 @@ router = APIRouter()
 )
 @cache(expire=60)
 async def person_search(
-        query: Optional[str] = Query('', description="Search query for person name"),
-        page_number: int = 1,
-        page_size: int = 50,
+        query: str = Query('', description="Search query for person name"),
+        page_size: int = Query(50, description="Number of persons per page"),
+        page_number: int = Query(1, description="Page number"),
         person_service: PersonService = Depends(person_service),
 ) -> list[PersonWithFilms]:
     found_persons = await person_service.person_search(
@@ -27,23 +27,32 @@ async def person_search(
         page_size=page_size,
         page_number=page_number
     )
+
+    if not found_persons:
+        raise HTTPException(status_code=404, detail=f"Person not found.")
+
     return found_persons
 
 
 @router.get(
     "/",
     response_model=list[PersonUUID],
-    summary="List of persons",
+    summary="List of all persons.",
 )
 @cache(expire=60)
 async def person(
-        person_service: PersonService = Depends(person_service),
-        page: int = 1,
-        page_size: int = 10
+        page_size: int = Query(50, description="Number of persons per page"),
+        page_number: int = Query(1, description="Page number"),
+        person_service: PersonService = Depends(person_service)
 ) -> list[PersonUUID]:
     person_list = await person_service.person_list(
-        page, page_size
+        page_size=page_size,
+        page_number=page_number
     )
+
+    if not person_list:
+        raise HTTPException(status_code=404, detail=f"There is no persons.")
+
     return person_list
 
 
@@ -54,16 +63,20 @@ async def person(
 )
 @cache(expire=60)
 async def persons(
-        person_id: str,
-        page_number: int = 1,
-        page_size: int = 50,
-        person_service: PersonService = Depends(person_service),
+        person_id: str = Path(..., description="The ID of person to find films with this person."),
+        page_size: int = Query(50, description="Number of persons per page"),
+        page_number: int = Query(1, description="Page number"),
+        person_service: PersonService = Depends(person_service)
 ) -> PersonUUID:
     person = await person_service.person_detail(
         person_id=person_id,
         page_size=page_size,
         page_number=page_number
     )
+
+    if not person:
+        raise HTTPException(status_code=404, detail=f"No person with ID: {person_id}")
+
     return person
 
 
@@ -85,7 +98,7 @@ async def films_with_person(
         page_size=page_size
     )
     if not films:
-        raise HTTPException(status_code=404, detail="No films found for this person")
+        raise HTTPException(status_code=404, detail="No films found with this person")
 
     return films
 
